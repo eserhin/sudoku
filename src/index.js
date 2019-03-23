45b09716c9bfdc1483b79656f5d32b7a206f6239
eserhin <47678732+eserhin@users.noreply.github.com>
2019-03-23 12:08:20 +0300

    First attempt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function solveSudoku(matrix) {
-  // your solution
+module.exports = function solveSudouku(matrix) {
+	  var z = matrix[0].length;
+    var x = matrix.length;
+    var g = [];
+    var u;
+    for (var i = 0;i<x;i++){
+      g.push([]);
+      for (var j = i;j<z;j++){
+        g[i].push(j); 
+        u=i;
+      }
+      var c =0;
+      while(g[u].length<z){
+			  g[u].push(c); 
+        c++;
+      } 
+    }
+    return g;
 }
